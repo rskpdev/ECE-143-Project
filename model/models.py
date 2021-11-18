179ea94e05e198960cadcfac76a14c9872961122
JerryWangjiawei <94089505+JerryWangjiawei@users.noreply.github.com>
2021-11-18 15:33:13 -0800

    Update models.py
    add confusion matrix plotting

--- model/models.py
+++ model/models.py
@@ -4,14 +4,20 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
 
 
 def Logistic_Regression_Model(X_train, Y_train, X_test, Y_test):
-    model = LogisticRegression()
+    model = LogisticRegression(solver='lbfgs', max_iter=1000)
     model.fit(X_train, Y_train)
     y_bar = model.predict(X_test)
     acc = accuracy_score(Y_test, y_bar)
     confusion = confusion_matrix(Y_test, y_bar)
+    dis = ConfusionMatrixDisplay(confusion_matrix=confusion, display_labels=model.classes_)
+    plt.figure()
+    dis.plot()
+    plt.show()
     return acc, confusion
 
 
@@ -21,6 +27,10 @@ def DecisionTree_Model(X_train, Y_train, X_test, Y_test):
     y_bar = model.predict(X_test)
     acc = accuracy_score(Y_test, y_bar)
     confusion = confusion_matrix(Y_test, y_bar)
+    dis = ConfusionMatrixDisplay(confusion_matrix=confusion, display_labels=model.classes_)
+    plt.figure()
+    dis.plot()
+    plt.show()
     return acc, confusion
 
 
@@ -30,6 +40,10 @@ def RandomForest_Model(X_train, Y_train, X_test, Y_test):
     y_bar = model.predict(X_test)
     acc = accuracy_score(Y_test, y_bar)
     confusion = confusion_matrix(Y_test, y_bar)
+    dis = ConfusionMatrixDisplay(confusion_matrix=confusion, display_labels=model.classes_)
+    plt.figure()
+    dis.plot()
+    plt.show()
     return acc, confusion
 
 
@@ -39,6 +53,10 @@ def SVM_Model(X_train, Y_train, X_test, Y_test):
     y_bar = model.predict(X_test)
     acc = accuracy_score(Y_test, y_bar)
     confusion = confusion_matrix(Y_test, y_bar)
+    dis = ConfusionMatrixDisplay(confusion_matrix=confusion, display_labels=model.classes_)
+    plt.figure()
+    dis.plot()
+    plt.show()
     return acc, confusion
 
 
@@ -48,4 +66,8 @@ def KNN_Model(X_train, Y_train, X_test, Y_test, n):
     y_bar = model.predict(X_test)
     acc = accuracy_score(Y_test, y_bar)
     confusion = confusion_matrix(Y_test, y_bar)
+    dis = ConfusionMatrixDisplay(confusion_matrix=confusion, display_labels=model.classes_)
+    plt.figure()
+    dis.plot()
+    plt.show()
     return acc, confusion
